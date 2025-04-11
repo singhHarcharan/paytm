fa7ada7f8789801c736623524032ea301cee0ff8
Hustler Harcharan <hustler_harcharan@Hustler-Harcharans-MacBook-Air.local>
2025-04-11 23:26:53 +0530

    implementation of update info by user

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,6 +4,7 @@ const zod = require('zod');
 const jwt = require('jsonwebtoken');
 const { User } = require('../db');
 const JWT_SECRET = require('../config');
+const authMiddleware = require('../middleware');
 
 const userSchema = zod.object({
     username: zod.string().min(3).max(30),
@@ -21,7 +22,6 @@ router.post("/signup", async (req, res) => {
             message: "Email already taken / Incorrect inputs"
         })
     }
-
     // check if user exists
     const existingUser = await User.findOne({
         username: bodyData.username
@@ -31,7 +31,6 @@ router.post("/signup", async (req, res) => {
             message: "User already exists"
         })
     }
-
     // create user
     const user = await User.create(bodyData);
     const userId = user._id;
@@ -40,6 +39,29 @@ router.post("/signup", async (req, res) => {
     req.json({
         message: "User created successfully",
         token: token,
+    })
+})
+
+const updateBody = zod.object( {
+    firstname: zod.string().optional(),
+    lastname: zod.string().optional(),
+    password: zod.string().optional(),
+})
+
+// Update Information
+router.put("/update", authMiddleware, async (req, res) => {
+    // check input validation using zod library
+    const bodyData = req.body;
+    const { success } = updateBody.safeParse(bodyData);
+    if (!success) {
+        res.status(411).json({
+            message: "Error while updating information"
+        })
+    }
+    // check if user exists
+    await User.updateOne({_id: req.userId}, bodyData);
+    req.json({
+        message: "User updated successfully"
     })
 })
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,5 @@
+// Link to the documentation where steps are described to solve this assignment - 
+// Link - https://projects.100xdevs.com/tracks/oAjvkeRNZThPMxZf4aX5/JLaLbhDuYn3h5Cn7WJu1
 
 function App() {
 
