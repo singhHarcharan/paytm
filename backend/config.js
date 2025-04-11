3dab5dad899e85890459a24866e98525946cd6c3
Hustler Harcharan <hustler_harcharan@Hustler-Harcharans-MacBook-Air.local>
2025-04-11 22:42:44 +0530

    added external dependencies to the app

---
+++ backend/config.js
@@ -0,0 +1,2 @@
+const JWT_SECRET = "harcharan";
+module.exports = JWT_SECRET;
--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const cors = require("cors");
 const mainRouter = require("./routes/index");
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 // Send every request to the main router
 app.use("api/v1", mainRouter);
+
+// listen on port 3000
+app.listen(3000);
 
 
