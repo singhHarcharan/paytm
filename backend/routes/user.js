d3bd2b3b0ad89346dae84ffffb46a42cb39fb089
Hustler Harcharan <hustler_harcharan@Hustler-Harcharans-MacBook-Air.local>
2025-04-11 22:58:59 +0530

    implemented signup route

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,4 +1,46 @@
 const express = require('express');
 const router = express.Router();
+const zod = require('zod');
+const jwt = require('jsonwebtoken');
+const { User } = require('../db');
+const JWT_SECRET = require('../config');
+
+const userSchema = zod.object({
+    username: zod.string().min(3).max(30),
+    password: zod.string().min(8),
+    firstname: zod.string().max(50),
+    lastname: zod.string().max(50),
+})
+
+// signup and signin routes
+router.post("/signup", async (req, res) => {
+    const bodyData = req.body;
+    const { success } = userSchema.safeParse(bodyData);
+    if(!success) {
+        req.json({
+            message: "Email already taken / Incorrect inputs"
+        })
+    }
+
+    // check if user exists
+    const existingUser = await User.findOne({
+        username: bodyData.username
+    })
+    if(existingUser._id) {
+        req.json({
+            message: "User already exists"
+        })
+    }
+
+    // create user
+    const user = await User.create(bodyData);
+    const userId = user._id;
+    const token = jwt.sign({ userId }, JWT_SECRET)
+
+    req.json({
+        message: "User created successfully",
+        token: token,
+    })
+})
 
 module.exports = router;
