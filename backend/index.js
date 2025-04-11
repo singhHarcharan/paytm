269e10116409832a98c5c2fb84859b38b58740a1
Hustler Harcharan <hustler_harcharan@Hustler-Harcharans-MacBook-Air.local>
2025-04-11 23:08:46 +0530

    implemented middleware for token verification

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const cors = require("cors");
-const mainRouter = require("./routes/index");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+const mainRouter = require("./routes/index");
 
 // Send every request to the main router
 app.use("api/v1", mainRouter);
